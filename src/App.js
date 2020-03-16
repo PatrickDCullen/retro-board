54969b1569fd1b9293c988ce31fc2227727b4704
PatrickDCullen <cullenpatrickd@gmail.com>
2020-03-16 11:27:37 -0400

    Add like and dislike functionality to cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
 
-  const [cardArray, setCardArray] = useState(["Enter text"]);
+  const [cardArray, setCardArray] = useState([{"text" : "Enter text", "likes" : 0, "dislikes" : 0}]);
+  // const [cards, setCards] = useState({"text": "Enter text"});
 
   return (
     <div className="App">
@@ -21,7 +22,9 @@ function App() {
           <h2>Went Well</h2>
           {/* Add a new card button */}
           <button type="button" className="ButtonAdd button button-default" aria-label="Add new card"
-            title="Add new card" onClick={() => setCardArray(cardArray.concat("Enter text"))}>+</button>
+            title="Add new card" onClick={() => {setCardArray(cardArray.concat({"text":"Enter text", "likes" : 0, "dislikes" : 0}));
+              // console.log(cardArray[0]["text"]);
+              }}>+</button>
 
           {/* A retro card (retrospective item) */}
           <div className="RetroCard" aria-label="Retro card">
@@ -30,9 +33,9 @@ function App() {
               return (
                 <Fragment key={`card-${index}`}>
                   <textarea className="textbox" placeholder="Enter text here" aria-label="Enter text here"
-                    rows="1" value={card} onChange={e => {
+                    rows="1" value={cardArray[index]["text"]} onChange={e => {
                       const cardArrayCopy = [...cardArray];
-                      cardArrayCopy[index] = e.target.value;
+                      cardArrayCopy[index]["text"] = e.target.value;
                       setCardArray(cardArrayCopy);
                     }}></textarea>
 
@@ -47,11 +50,21 @@ function App() {
                       <img src="timesCircle.svg" alt="Delete" width="12" height="12"/>
                     </button>
                     <div>
-                      <button type="button" className="button button-left" title="Like">
-                        <img src="thumbsUp.svg" alt="Like" width="12" height="12"/> 5
+                      <button type="button" className="button button-left" title="Like" onClick={() => {
+                        const cardArrayCopy = [...cardArray];
+                        // console.log(cardArrayCopy[index]["likes"]);
+                        cardArrayCopy[index]["likes"]++;
+                        setCardArray(cardArrayCopy);
+                      }}>
+                        <img src="thumbsUp.svg" alt="Like" width="12" height="12"/> {card["likes"]}
                       </button>
-                      <button type="button" className="button button-left" title="Dislike">
-                        <img src="thumbsDown.svg" alt="Dislike" width="12" height="12"/> 0
+                      <button type="button" className="button button-left" title="Dislike" onClick={() => {
+                        const cardArrayCopy = [...cardArray];
+                        // console.log(cardArrayCopy[index]["likes"]);
+                        cardArrayCopy[index]["dislikes"]++;
+                        setCardArray(cardArrayCopy);
+                      }}>
+                        <img src="thumbsDown.svg" alt="Dislike" width="12" height="12"/> {card["dislikes"]}
                       </button>
                       <button type="button" className="button button-right" title="Move right">
                         <img src="angleRight.svg" alt="Move right" width="12" height="12"/>
