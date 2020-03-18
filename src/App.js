ecb2ad961e9594fad7a2079e7f0f9d74e1802943
PatrickDCullen <cullenpatrickd@gmail.com>
2020-03-18 09:46:59 -0400

    Add functionality of second category

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import thumbsDown from './thumbsDown.svg';
 
 function App() {
 
-  const [cardArray, setCardArray] = useState([{"text" : "Enter text", "likes" : 0, "dislikes" : 0}]);
+  const [cardArray, setCardArray] = useState([]);
   // const [cards, setCards] = useState({"text": "Enter text"});
 
   return (
@@ -27,16 +27,81 @@ function App() {
           <h2>Went Well</h2>
           {/* Add a new card button */}
           <button type="button" className="ButtonAdd button button-default" aria-label="Add new card"
-            title="Add new card" onClick={() => {setCardArray(cardArray.concat({"text":"Enter text", "likes" : 0, "dislikes" : 0}));
+            title="Add new card" onClick={() => {setCardArray(cardArray.concat({"category" : "wentWell", "text":"Enter text", "likes" : 0, "dislikes" : 0}));
               // console.log(cardArray[0]["text"]);
               }}>+</button>
 
           {/* A retro card (retrospective item) */}
           <div className="RetroCard" aria-label="Retro card">
             {/* User input */}
-            {cardArray.map((card,index) => {
+            {//cardArray.filter(card => card["category"] === "wentWell")
+            cardArray.map((card,index) => {
+              if (card["category"] === "wentWell") {
+                return (
+                <Fragment key={`card1-${index}`}>
+                  <textarea className="textbox" placeholder="Enter text here" aria-label="Enter text here"
+                    rows="1" value={cardArray[index]["text"]} onChange={e => {
+                      console.log("hi1");
+                      const cardArrayCopy = [...cardArray];
+                      cardArrayCopy[index]["text"] = e.target.value;
+                      setCardArray(cardArrayCopy);
+                    }}></textarea>
+
+                  <div className="button-group" >
+                    <button type="button" className="button button-left" title="Move left">
+                      <img src={angleLeft} alt="Move left" width="12" height="12"/>
+                    </button>
+                    <button type="button" className="button button-delete" title="Delete" onClick={() => {
+                      const filteredArray = cardArray.filter((card, currentIndex) => currentIndex !== index);
+                      setCardArray(filteredArray);
+                    }}>
+                      <img src={timesCircle} alt="Delete" width="12" height="12"/>
+                    </button>
+                    <div>
+                      <button type="button" className="button button-left" title="Like" onClick={() => {
+                        const cardArrayCopy = [...cardArray];
+                        // console.log(cardArrayCopy[index]["likes"]);
+                        cardArrayCopy[index]["likes"]++;
+                        setCardArray(cardArrayCopy);
+                      }}>
+                        <img src={thumbsUp} alt="Like" width="12" height="12"/> {card["likes"]}
+                      </button>
+                      <button type="button" className="button button-left" title="Dislike" onClick={() => {
+                        const cardArrayCopy = [...cardArray];
+                        // console.log(cardArrayCopy[index]["likes"]);
+                        cardArrayCopy[index]["dislikes"]++;
+                        setCardArray(cardArrayCopy);
+                      }}>
+                        <img src={thumbsDown} alt="Dislike" width="12" height="12"/> {card["dislikes"]}
+                      </button>
+                      <button type="button" className="button button-right" title="Move right">
+                        <img src={angleRight} alt="Move right" width="12" height="12"/>
+                      </button>
+                    </div>
+                  </div>
+                </Fragment>
+            );
+            }
+            })}
+          </div>
+        </div>
+
+        {/* Retro category */}
+        <div className="RetroCategory RetroCategory-2">
+          <h2>To Improve</h2>
+          <button type="button" className="ButtonAdd button button-default" aria-label="Add new card"
+            title="Add new card" onClick={() => {setCardArray(cardArray.concat({"category" : "toImprove", "text":"Enter text", "likes" : 0, "dislikes" : 0}));
+              // console.log(cardArray[0]["text"]);
+              }}>+</button>
+
+          {/* A retro card (retrospective item) */}
+          <div className="RetroCard" aria-label="Retro card">
+            {/* User input */}
+            {//cardArray.filter(card => card["category"] === "toImprove").
+            cardArray.map((card,index) => {
+              if (card["category"] === "toImprove") {
               return (
-                <Fragment key={`card-${index}`}>
+                <Fragment key={`card2-${index}`}>
                   <textarea className="textbox" placeholder="Enter text here" aria-label="Enter text here"
                     rows="1" value={cardArray[index]["text"]} onChange={e => {
                       const cardArrayCopy = [...cardArray];
@@ -78,15 +143,9 @@ function App() {
                   </div>
                 </Fragment>
             );
+            }
             })}
           </div>
-        </div>
-
-        {/* Retro category */}
-        <div className="RetroCategory RetroCategory-2">
-          <h2>To Improve</h2>
-          <button type="button" className="ButtonAdd button button-default" aria-label="Add to new card"
-            title="Add to new card">+</button>
         </div>
 
         {/* Retro category */}
